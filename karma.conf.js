34488eabe497f008785230555ce0d4a0cda7e6c2
Levon Arakelyan <levonarakelyan16102002@gmail.com>
2021-12-04 18:50:14 +0400

    Changed app name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/myApp'),
+      dir: require('path').join(__dirname, './coverage/TicTacToeWeb'),
       subdir: '.',
       reporters: [
         { type: 'html' },
